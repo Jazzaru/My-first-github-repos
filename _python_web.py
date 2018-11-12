ba8c0d75e61a94629329e7c7a83e317c80db0b31
Jazzaru <44984617+Jazzaru@users.noreply.github.com>
2018-11-12 17:03:19 -0500

    Add files via upload
    main python file

---
+++ _python_web.py
@@ -0,0 +1,16 @@
+"""
+Develeper(s): JHL(Jeremy)
+FROM:jeremyables42@yahoo.com
+_Tested in Windows 10
+
+"""
+
+import os
+import time
+import webbrowser
+
+def main(url1):
+    print "opening google from python!"
+    url = url1
+    webbrowser.open(url)
+main("www.google.com")
